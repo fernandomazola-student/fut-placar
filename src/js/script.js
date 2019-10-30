729f8257b286a8e328fc6efab0490fcc1214d6d6
fernandomazola <fernando.mazola@profite.com.br>
2019-10-30 13:57:57 -0300

    Iniciando projeto futebol placar

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+times = {
+  name: 'Corinthians',
+  points: 10,
+  wingame: 10,
+  losegame: 0,
+  gols: 100
+}
+var btnJogar = document.querySelector('.jogar');
+
+function renderTimes() {
+  var div = document.createElement('div');
+}
